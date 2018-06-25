e508f494d25e97816839915eb8225c429f8d9ed6
eeeeeta <eeeeeta@users.noreply.github.com>
2018-06-25 15:41:54 +0100

    use a non-terrible normalization thingie

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 //! Helpful utility functions.
-use huawei_modem::pdu::{PduAddress, AddressType};
+use huawei_modem::pdu::{PduAddress, AddressType, HexData, PhoneNumber};
 use huawei_modem::convert::TryFrom;
 
 pub type Result<T> = ::std::result::Result<T, ::failure::Error>;
@@ -7,22 +7,21 @@ pub type Result<T> = ::std::result::Result<T, ::failure::Error>;
 pub fn normalize_address(addr: &PduAddress) -> String {
     let ton: u8 = addr.type_addr.into();
     let mut ret = format!("{:02X}", ton);
-    for b in addr.number.0.iter() {
-        ret += &format!("{}", b);
-    }
+    ret += &HexData(&addr.number.0).to_string();
     ret
 }
 pub fn un_normalize_address(addr: &str) -> Option<PduAddress> {
     if addr.len() < 3 {
         return None;
     }
-    let toa = u8::from_str_radix(&addr[0..2], 16).ok()?;
+    let mut data = HexData::decode(addr).ok()?;
+    let toa = data.remove(0);
     let toa = AddressType::try_from(toa).ok()?;
-    let mut addr: PduAddress = addr.parse().unwrap();
-    addr.number.0.remove(0);
-    addr.number.0.remove(0);
-    addr.type_addr = toa;
-    Some(addr)
+    let num = PhoneNumber(data);
+    Some(PduAddress {
+        type_addr: toa,
+        number: num
+    })
 }
 pub fn make_nick_for_address(addr: &PduAddress) -> String {
     let mut ret = "S".to_string();
